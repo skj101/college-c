44f3ec0f91aa4a25a4e1cd7d03c0b540d4568103
sanu k joseph <73690984+sanukjoseph@users.noreply.github.com>
2023-12-12 07:12:13 +0530

    Update binary-search-tree.c

--- CYCLE_2/binary-search-tree.c
+++ CYCLE_2/binary-search-tree.c
@@ -1,103 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a Node in the Binary Search Tree
-struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-};
+#define MAX_SIZE 100
 
-// Function to create a new Node
-struct Node* createNode(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->left = newNode->right = NULL;
-    return newNode;
+int tree[MAX_SIZE];
+int size = 0;
+
+int compareInt(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
 }
 
-// Function to insert a value into the BST
-struct Node* insert(struct Node* root, int value) {
-    if (root == NULL)
-        return createNode(value);
-
-    if (value < root->data)
-        root->left = insert(root->left, value);
-    else if (value > root->data)
-        root->right = insert(root->right, value);
-
-    return root;
-}
-
-// Function to find the minimum value in the BST
-struct Node* findMin(struct Node* node) {
-    struct Node* current = node;
-    while (current && current->left != NULL)
-        current = current->left;
-    return current;
-}
-
-// Function to delete a value from the BST
-struct Node* deleteNode(struct Node* root, int value) {
-    if (root == NULL)
-        return root;
-
-    if (value < root->data)
-        root->left = deleteNode(root->left, value);
-    else if (value > root->data)
-        root->right = deleteNode(root->right, value);
-    else {
-        if (root->left == NULL) {
-            struct Node* temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            struct Node* temp = root->left;
-            free(root);
-            return temp;
-        }
-
-        struct Node* temp = findMin(root->right);
-        root->data = temp->data;
-        root->right = deleteNode(root->right, temp->data);
-    }
-
-    return root;
-}
-
-// Function to search for a value in the BST
-struct Node* search(struct Node* root, int value) {
-    if (root == NULL || root->data == value)
-        return root;
-
-    if (value < root->data)
-        return search(root->left, value);
-
-    return search(root->right, value);
-}
-
-// Function to perform an in-order traversal of the BST
-void inOrderTraversal(struct Node* root) {
-    if (root != NULL) {
-        inOrderTraversal(root->left);
-        printf("%d ", root->data);
-        inOrderTraversal(root->right);
-    }
-}
-
-// Main function
 int main() {
-    struct Node* root = NULL;
     int choice, value;
 
     do {
-        printf("\nBinary Search Tree Operations:\n");
-        printf("1. Insert\n");
-        printf("2. Delete\n");
-        printf("3. Search\n");
-        printf("4. In-order Traversal\n");
-        printf("5. Quit\n");
-
+        printf("\nBinary Search Tree Operations:\n1. Insert\n2. Delete\n3. Search\n4. In-order Traversal\n5. Quit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -105,28 +22,47 @@ int main() {
             case 1:
                 printf("Enter value to insert: ");
                 scanf("%d", &value);
-                root = insert(root, value);
+                if (size == MAX_SIZE || bsearch(&value, tree, size, sizeof(int), compareInt)) {
+                    printf("Duplicate value or tree is full. Not inserted.\n");
+                } else {
+                    tree[size++] = value;
+                    printf("%d inserted into the tree.\n", value);
+                }
                 break;
 
             case 2:
                 printf("Enter value to delete: ");
                 scanf("%d", &value);
-                root = deleteNode(root, value);
+                {
+                    int* found = bsearch(&value, tree, size, sizeof(int), compareInt);
+                    if (found) {
+                        *found = tree[size-- - 1];
+                        printf("%d deleted from the tree.\n", value);
+                    } else {
+                        printf("%d not found in the tree.\n", value);
+                    }
+                }
                 break;
 
             case 3:
                 printf("Enter value to search: ");
                 scanf("%d", &value);
-                struct Node* result = search(root, value);
-                if (result != NULL)
-                    printf("%d found in the tree.\n", value);
-                else
-                    printf("%d not found in the tree.\n", value);
+                {
+                    int* found = bsearch(&value, tree, size, sizeof(int), compareInt);
+                    if (found) {
+                        printf("%d found in the tree.\n", value);
+                    } else {
+                        printf("%d not found in the tree.\n", value);
+                    }
+                }
                 break;
 
             case 4:
                 printf("In-order Traversal: ");
-                inOrderTraversal(root);
+                qsort(tree, size, sizeof(int), compareInt);
+                for (int i = 0; i < size; i++) {
+                    printf("%d ", tree[i]);
+                }
                 printf("\n");
                 break;
 
